c69804c412180db69e7afe1c8f7488c099b04e58
João Carvalho <joao.pedro.carvalho@tecnico.ulisboa.pt>
2014-12-30 16:13:10 +0000

    Show responsible professors first on the teacher list

--- src/main/java/org/fenixedu/learning/domain/executionCourse/components/InitialPageComponent.java
+++ src/main/java/org/fenixedu/learning/domain/executionCourse/components/InitialPageComponent.java
@@ -1,14 +1,17 @@
 package org.fenixedu.learning.domain.executionCourse.components;
 
-import org.fenixedu.academic.domain.ExecutionCourse;
+import java.util.Comparator;
+import java.util.stream.Collectors;
 
+import org.fenixedu.academic.domain.ExecutionCourse;
+import org.fenixedu.academic.domain.Professorship;
 import org.fenixedu.bennu.core.domain.User;
 import org.fenixedu.bennu.core.security.Authenticate;
 import org.fenixedu.cms.domain.Page;
 import org.fenixedu.cms.domain.component.CMSComponent;
 import org.fenixedu.cms.domain.component.ComponentType;
-import org.fenixedu.learning.domain.executionCourse.ExecutionCourseSite;
 import org.fenixedu.cms.rendering.TemplateContext;
+import org.fenixedu.learning.domain.executionCourse.ExecutionCourseSite;
 
 @ComponentType(name = "InitialPage", description = "Provides the information needed for the initial page of an Execution Course")
 public class InitialPageComponent implements CMSComponent {
@@ -17,7 +20,13 @@ public class InitialPageComponent implements CMSComponent {
     @Override
     public void handle(Page page, TemplateContext componentContext, TemplateContext globalContext) {
         ExecutionCourse executionCourse = ((ExecutionCourseSite) page.getSite()).getExecutionCourse();
-        globalContext.put("professorships", executionCourse.getProfessorshipsSortedAlphabetically());
+        globalContext.put(
+                "professorships",
+                executionCourse
+                        .getProfessorshipsSet()
+                        .stream()
+                        .sorted(Comparator.comparing(Professorship::isResponsibleFor).reversed()
+                                .thenComparing(Professorship.COMPARATOR_BY_PERSON_NAME)).collect(Collectors.toList()));
         globalContext.put("isStudent", isStudent(Authenticate.getUser()));
         globalContext.put("executionCourse", executionCourse);
     }
