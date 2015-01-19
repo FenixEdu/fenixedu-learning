fbfbc7203f249195dc045b0f36d33d90f0a95650
João Carvalho <joao.pedro.carvalho@tecnico.ulisboa.pt>
2015-01-19 19:16:55 +0000

    Added licensing headers

--- src/main/java/org/fenixedu/learning/domain/degree/components/DescriptionComponent.java
+++ src/main/java/org/fenixedu/learning/domain/degree/components/DescriptionComponent.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright © 2015 Instituto Superior Técnico
+ *
+ * This file is part of FenixEdu Learning.
+ *
+ * FenixEdu Learning is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FenixEdu Learning is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with FenixEdu Learning.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.fenixedu.learning.domain.degree.components;
 
 import java.util.Collection;
