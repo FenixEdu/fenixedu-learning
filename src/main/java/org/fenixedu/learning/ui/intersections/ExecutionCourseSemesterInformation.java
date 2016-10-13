8cc6a9addca73a1c0e56d84a5b4ecd1366bf01ea
diutsu <sousago@gmail.com>
2016-10-13 10:51:19 +0100

    add modular render to show extra site info LRN-50 #resolve

---
+++ src/main/java/org/fenixedu/learning/ui/intersections/ExecutionCourseSemesterInformation.java
@@ -0,0 +1,14 @@
+package org.fenixedu.learning.ui.intersections;
+
+import org.fenixedu.bennu.rendering.annotations.BennuIntersection;
+import org.fenixedu.bennu.rendering.annotations.BennuIntersections;
+
+/**
+ * Created by diutsu on 13/10/16.
+ */
+@BennuIntersections({
+        @BennuIntersection(location = "site.extra", position = "description",
+                file= "/templates/fenixedu-learning/siteExecutionCourseSemester.html")
+})
+public class ExecutionCourseSemesterInformation {
+}
